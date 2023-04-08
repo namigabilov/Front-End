b53efdc8872b392702b4b9354b2e47fe044877de
namigabilov <namigna@code.edu.az>
2023-04-08 13:43:40 +0400

    mani-contairer And Posts Area Created

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -1,8 +1,31 @@
 $( document ).ready(function() {
+  $('.like').on('click', function() {
+    $(this).toggleClass('fa-regular fa-solid');
+    if ($(this).hasClass('fa-solid')) {
+      $(this).addClass('activeForLike');
+    } else {
+      $(this).removeClass('activeForLike');
+    }
+  });
+
+      $('.savePost').on('click', function() {
+        $(this).toggleClass('fa-regular fa-solid');
+      });
     $('.navItems').on('click',function (e) {
-        console.log("salam");
         e.preventDefault();
         $('.navItems').removeClass('active')
         $(this).addClass('active');
     })
+    $('#settings').on('click', function () {
+        if ($('#settingTable').hasClass('d-none')) {
+          $('#settingTable').removeClass('d-none');
+        } else {
+          $('#settingTable').addClass('d-none');
+        }
+    });
+    $(document).on('click', function(e) {
+        if (!$('.navigation, #settings').has(e.target).length) {
+          $('#settingTable').addClass('d-none');
+        }
+      });
 });
