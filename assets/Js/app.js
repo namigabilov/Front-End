e167e920187e0f77b055e010c9a8bf56f3dcb83d
namigabilov <namigna@code.edu.az>
2023-04-12 23:35:32 +0400

    Mobile Chat Direcet Reels Profile Updated

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -1,4 +1,5 @@
 $( document ).ready(function() {
+  
   $('.like').on('click', function() {
   $(this).toggleClass('fa-regular fa-solid');
   if ($(this).hasClass('fa-solid')) {
@@ -28,7 +29,128 @@ $( document ).ready(function() {
       }
   });
   $("#myModal").modal('show');
-});
-feather.replace();
+  function tabChange() {
+    var tabs = $('.nav-tabs > li');
+    var active = tabs.filter('.active');
+    var next = active.next('li').length? active.next('li').find('a') : tabs.filter(':first-child').find('a');
+    next.tab('show');
+  }
 
+  $('.tab-pane').hover(function() {
+    clearInterval(tabCycle);
+  }, function() {
+    tabCycle = setInterval(tabChange, 5000);
+  });
+  var tabCycle = setInterval(tabChange, 5000)
+  $(function(){
+    $('.nav-tabs a').click(function(e) {
+        e.preventDefault();
+        clearInterval(tabCycle);
+        $(this).tab('show')
+        tabCycle = setInterval(tabChange, 5000);
+    });
+});
+});
+const input = document.querySelector(".input");
+const messages = document.querySelector(".messages");
+
+let initial = true,
+  last = false;
+function message(msg, self = false, animate = true) {
+  // check whether to insert a spacer
+  if (initial) {
+    initial = false;
+    last = self;
+  } else if (this.last !== self) {
+    this.last = self;
+    const spacer = document.createElement("div");
+    spacer.classList.add("spacer");
+    messages.appendChild(spacer);
+  }
+
+  // append message
+  const message = document.createElement("div");
+  message.classList.add("message");
+  self && message.classList.add("self");
+  animate && message.classList.add("animate");
+  const text = document.createTextNode(msg);
+  message.appendChild(text);
+
+  messages.appendChild(message);
+  messages.scrollTop = messages.scrollHeight;
+
+  this.last = self;
+}
+
+// setup some intial text
+((messages) =>
+  messages.map(({ text, self = false }) => {
+    message(text, self, false);
+  }))([
+  { text: "Salam" },
+  { text: "Bu Yazılmış Default Text'dir "},
+  { text: "Salam", self: true },
+  { text: "Bu yazılmış Default Cavabdır" , self :true},
+
+]);
+
+let timer = null;
+input.addEventListener("keyup", (e) => {
+  if (e.key !== "Enter" || !input.value.trim().length) return;
+  message(input.value, true);
+  input.value = "";
+
+  // avoid multiple responses from last message
+  clearTimeout(timer);
+
+  // generate random response
+  if (Math.random() > 0.3) {
+    const responses = [
+      "It is certain.",
+      "It is decidedly so.",
+      "Without a doubt.",
+      "Yes definitely.",
+      "You may rely on it.",
+      "As I see it, yes.",
+      "Most likely.",
+      "Outlook good.",
+      "Yes.",
+      "Signs point to yes.",
+      "Reply hazy, try again.",
+      "Ask again later.",
+      "Better not tell you now.",
+      "Cannot predict now.",
+      "Concentrate and ask again.",
+      "Don't count on it.",
+      "My reply is no.",
+      "My sources say no.",
+      "Outlook not so good.",
+      "Very doubtful."
+    ];
+    // random response delay
+    timer = setTimeout(
+      () => message(responses[(Math.random() * responses.length) | 0]),
+      (Math.random() * 3000) | 0
+    );
+  }
+});
+
+// remove weird overflow on animation
+let overflown = false;
+messages.addEventListener("animationend", () => {
+  if (overflown) return;
+  if (messages.scrollHeight > messages.clientHeight) {
+    messages.style["overflow-y"] = "auto";
+    overflown = true;
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (messages.scrollHeight > messages.clientHeight) {
+    messages.style["overflow-y"] = "auto";
+    overflown = true;
+  } else overflown = false;
+});
+
+feather.replace();
 document.querySelector(".nav-item").focus();
