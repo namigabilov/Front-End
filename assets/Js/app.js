f9a72afaa1d5e0600437c525d503618565328b04
namigabilov <namigna@code.edu.az>
2023-04-13 19:17:06 +0400

    Profile, direct chat updated Mobile Version

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -88,7 +88,7 @@ function message(msg, self = false, animate = true) {
     message(text, self, false);
   }))([
   { text: "Salam" },
-  { text: "Bu Yazılmış Default Text'dir "},
+  { text: "Bu Yazılmış Default Mesaj'dir "},
   { text: "Salam", self: true },
   { text: "Bu yazılmış Default Cavabdır" , self :true},
 
@@ -104,35 +104,14 @@ input.addEventListener("keyup", (e) => {
   clearTimeout(timer);
 
   // generate random response
-  if (Math.random() > 0.3) {
-    const responses = [
-      "It is certain.",
-      "It is decidedly so.",
-      "Without a doubt.",
-      "Yes definitely.",
-      "You may rely on it.",
-      "As I see it, yes.",
-      "Most likely.",
-      "Outlook good.",
-      "Yes.",
-      "Signs point to yes.",
-      "Reply hazy, try again.",
-      "Ask again later.",
-      "Better not tell you now.",
-      "Cannot predict now.",
-      "Concentrate and ask again.",
-      "Don't count on it.",
-      "My reply is no.",
-      "My sources say no.",
-      "Outlook not so good.",
-      "Very doubtful."
-    ];
-    // random response delay
-    timer = setTimeout(
-      () => message(responses[(Math.random() * responses.length) | 0]),
-      (Math.random() * 3000) | 0
-    );
-  }
+  const responses = [
+    "Salam Mən Mesaj Botuyam Ancaq Static Cavab verə bilirəm !",
+  ];
+  // random response delay
+  timer = setTimeout(
+    () => message(responses[(Math.random() * responses.length) | 0]),
+    (Math.random() * 3000) | 0
+  );
 });
 
 // remove weird overflow on animation
