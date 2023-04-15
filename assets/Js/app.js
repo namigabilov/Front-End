e0d3aa79f098779352a3b25d68498c364ce9eeec
namigabilov <namigna@code.edu.az>
2023-04-15 17:34:01 +0400

    Added Myprofile Desktop

--- assets/Js/app.js
+++ assets/Js/app.js
@@ -1,5 +1,12 @@
 $( document ).ready(function() {
-  
+  $(".chat-list a").click(function() {
+    $(".chatbox").addClass('showbox');
+    return false;
+});
+
+$(".chat-icon").click(function() {
+    $(".chatbox").removeClass('showbox');
+});
   $('.like').on('click', function() {
   $(this).toggleClass('fa-regular fa-solid');
   if ($(this).hasClass('fa-solid')) {
@@ -12,7 +19,6 @@ $( document ).ready(function() {
       $(this).toggleClass('fa-regular fa-solid');
   });
   $('.navItems').on('click',function (e) {
-      e.preventDefault();
       $('.navItems').removeClass('active')
       $(this).addClass('active');
   })
@@ -51,85 +57,5 @@ $( document ).ready(function() {
     });
 });
 });
-const input = document.querySelector(".input");
-const messages = document.querySelector(".messages");
-
-let initial = true,
-  last = false;
-function message(msg, self = false, animate = true) {
-  // check whether to insert a spacer
-  if (initial) {
-    initial = false;
-    last = self;
-  } else if (this.last !== self) {
-    this.last = self;
-    const spacer = document.createElement("div");
-    spacer.classList.add("spacer");
-    messages.appendChild(spacer);
-  }
-
-  // append message
-  const message = document.createElement("div");
-  message.classList.add("message");
-  self && message.classList.add("self");
-  animate && message.classList.add("animate");
-  const text = document.createTextNode(msg);
-  message.appendChild(text);
-
-  messages.appendChild(message);
-  messages.scrollTop = messages.scrollHeight;
-
-  this.last = self;
-}
-
-// setup some intial text
-((messages) =>
-  messages.map(({ text, self = false }) => {
-    message(text, self, false);
-  }))([
-  { text: "Salam" },
-  { text: "Bu Yazılmış Default Mesaj'dir "},
-  { text: "Salam", self: true },
-  { text: "Bu yazılmış Default Cavabdır" , self :true},
-
-]);
-
-let timer = null;
-input.addEventListener("keyup", (e) => {
-  if (e.key !== "Enter" || !input.value.trim().length) return;
-  message(input.value, true);
-  input.value = "";
-
-  // avoid multiple responses from last message
-  clearTimeout(timer);
-
-  // generate random response
-  const responses = [
-    "Salam Mən Mesaj Botuyam Ancaq Static Cavab verə bilirəm !",
-  ];
-  // random response delay
-  timer = setTimeout(
-    () => message(responses[(Math.random() * responses.length) | 0]),
-    (Math.random() * 3000) | 0
-  );
-});
-
-// remove weird overflow on animation
-let overflown = false;
-messages.addEventListener("animationend", () => {
-  if (overflown) return;
-  if (messages.scrollHeight > messages.clientHeight) {
-    messages.style["overflow-y"] = "auto";
-    overflown = true;
-  }
-});
-
-window.addEventListener("resize", () => {
-  if (messages.scrollHeight > messages.clientHeight) {
-    messages.style["overflow-y"] = "auto";
-    overflown = true;
-  } else overflown = false;
-});
-
 feather.replace();
 document.querySelector(".nav-item").focus();
